63385e1d038e77cae797cebc97dbcad916d35f0b
mova <valeriy.molchanov.77@gmail.com>
2020-08-30 09:26:00 +0300

    .proto + *.pb.go

---
+++ pkg/ssp/compile-proto.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -ex
+
+dir=$(dirname $0)
+
+# protoc \
+#   --proto_path "${dir}" \
+#   --go_out=Mgrpc/service_config/service_config.proto=/internal/proto/grpc_service_config:"${dir}" \
+#   --go-grpc_out=Mgrpc/service_config/service_config.proto=/internal/proto/grpc_service_config:"${dir}" \
+#   --go_opt=paths=source_relative \
+#   --go-grpc_opt=paths=source_relative \
+#   "${dir}"/ssp.proto
+
+protoc \
+  --proto_path "${dir}" \
+  --go_out="${dir}" \
+  --go-grpc_out="${dir}" \
+  --go_opt=paths=source_relative \
+  --go-grpc_opt=paths=source_relative \
+  "${dir}"/ssp.proto
